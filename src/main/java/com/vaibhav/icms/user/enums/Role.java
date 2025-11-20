61f978c87765a0dcb442128b0c30b8091d13ab2d
vibehav <vibehav@protonmail.com>
2025-11-20 14:56:36 +0530

    roles created and db repo implemented

---
+++ src/main/java/com/vaibhav/icms/user/enums/Role.java
@@ -0,0 +1,11 @@
+package com.vaibhav.icms.user.enums;
+
+public enum Role {
+    ADMIN,
+    PROJECT_MANAGER,
+    SITE_ENGINEER,
+    SAFETY_OFFICER,
+    ACCOUNTANT,
+    STORE_KEEPER,
+    COTRACTOR
+}
---
+++ src/main/java/com/vaibhav/icms/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.vaibhav.icms.user.repository;
+
+import org.springframework.stereotype.Repository; // Importing Repository annotation 🔴
+import com.vaibhav.icms.user.entity.User;  // Assuming there is a User entity class in this package 🟠
+import org.springframework.data.jpa.repository.JpaRepository; // Importing JpaRepository 🟠
+
+
+//This interface allows Spring to auto-generate all CRUD queries for User.
+@Repository // 🔴
+public interface UserRepository extends JpaRepository<User, Long> { // 🟠 🟠
+
+    User findByEmail(String email); // Method to find a user by email 🟠
+}
